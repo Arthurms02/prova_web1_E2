0275e4fb1971aa6717445b459b8cd0ab35654ea2
Arthur Marques <arthursouza@ads.fiponline.edu.br>
2025-10-31 19:13:16 -0300

    criando fixtures

---
+++ fixtures/questoes.js
@@ -0,0 +1,182 @@
+[
+{
+  "model": "hometask.questao",
+  "pk": "0676b4d4-7df1-4b9f-9278-82c6c31ab0bd",
+  "fields": {
+    "created_at": "2025-10-31T21:21:51.536Z",
+    "updated_at": "2025-10-31T21:21:51.536Z",
+    "enunciado": "(Enem PPL 2016) A charge faz alusão à intensa rivalidade entre as duas maiores potências do século XX. O momento mais tenso dessa disputa foi provocado pela",
+    "complemento": "",
+    "alternativa_a": "Ampliação da Guerra do Vietnã.",
+    "alternativa_b": "Construção do muro de Berlim.",
+    "alternativa_c": "Instalação de mísseis em Cuba.",
+    "alternativa_d": "Eclosão da Guerra dos Sete Dias.",
+    "alternativa_e": "Invasão do território do Afeganistão.",
+    "materia": 2,
+    "autor": 2,
+    "resposta_correta": "C"
+  }
+},
+{
+  "model": "hometask.questao",
+  "pk": "22dc771a-6935-4b49-ae0e-6877858e0046",
+  "fields": {
+    "created_at": "2025-10-31T21:14:32.695Z",
+    "updated_at": "2025-10-31T21:14:32.695Z",
+    "enunciado": "(Enem Digital 2020) O gesto representado no quadro simboliza uma diferença entre o império napoleônico e a monarquia absolutista, por",
+    "complemento": "complementos/questao_2025-10-31 21:14:32.695230+00:00/e519f3e465594a84bb049fc3cbbffeb0.png",
+    "alternativa_a": "Reduzir a autoridade de clero.",
+    "alternativa_b": "Instaurar a censura da imprensa.",
+    "alternativa_c": "Controlar a organização judiciária.",
+    "alternativa_d": "Suspender os pensões da nobreza.",
+    "alternativa_e": "Desrespeitar a propriedade privada.",
+    "materia": 2,
+    "autor": 2,
+    "resposta_correta": "A"
+  }
+},
+{
+  "model": "hometask.questao",
+  "pk": "28e75a8e-197e-4b05-aa2b-2e12568e927b",
+  "fields": {
+    "created_at": "2025-10-31T20:29:21.642Z",
+    "updated_at": "2025-10-31T20:29:21.642Z",
+    "enunciado": "(Enem PPL 2013) É preciso ressaltar que, de todas as capitanias brasileiras, Minas era a mais urbanizada. Não havia ali hegemonia de um ou dois grandes centros. A região era repleta de vilas e arraiais, grandes e pequenos, em cujas ruas muita gente circulava. As regiões da América portuguesa tiveram distintas lógicas de ocupação. Uma explicação para a especificidade da região descrita no texto está identificada na",
+    "complemento": "",
+    "alternativa_a": "Apropriação cultural diante das influências externas.",
+    "alternativa_b": "Produção manufatureira diante do exclusivo comercial.",
+    "alternativa_c": "Insubordinação religiosa diante da hierarquia eclesiástica.",
+    "alternativa_d": "Fiscalização estatal diante das particularidades econômicas.",
+    "alternativa_e": "Autonomia administrativa diante das instituições metropolitanas.",
+    "materia": 2,
+    "autor": 2,
+    "resposta_correta": "D"
+  }
+},
+{
+  "model": "hometask.questao",
+  "pk": "58880eca-0b06-4839-9590-3b2d3db4b8ff",
+  "fields": {
+    "created_at": "2025-10-31T19:33:09.092Z",
+    "updated_at": "2025-10-31T19:39:36.674Z",
+    "enunciado": "(Enem 2022) A biomassa celulósica pode ser utilizada para a produção de etanol de segunda geração. Entretanto, é necessario que os polissacarídeos sejam convertidos em mono e dissacarideos, processo que pode ser conduzido em meio ácido. conforme mostra o esquema, Nessa conversão de polissacarídeos, a função do íon H+ é:",
+    "complemento": "complementos/questao_2025-10-31 19:33:09.092414+00:00/bb7e7c3cb29d469fb30131394791525b.png",
+    "alternativa_a": "Dissolver os reagentes.",
+    "alternativa_b": "Deslocar o equilíbrio químico.",
+    "alternativa_c": "Aumentar a velocidade da reação.",
+    "alternativa_d": "Mudar a constante de equilíbrio da reação.",
+    "alternativa_e": "Formar ligações de hidrogênio como o polissacarídeos.",
+    "materia": 1,
+    "autor": 2,
+    "resposta_correta": "C"
+  }
+},
+{
+  "model": "hometask.questao",
+  "pk": "69ceb85b-abb3-4076-abc7-6d34f5682b7c",
+  "fields": {
+    "created_at": "2025-10-31T20:26:02.473Z",
+    "updated_at": "2025-10-31T20:26:02.473Z",
+    "enunciado": "(Enem PPL 2020) A originalidade do Absolutismo português talvez esteja no fato de ter sido o regime político europeu que melhor sintetizou a ideia do patrimonialismo estatal: os recursos materiais da nação se confundindo com os bens pessoais do monarca. Na colonização do Brasil, o patrimonialismo da Coroa portuguesa ficou evidente",
+    "complemento": "",
+    "alternativa_a": "Nas capitanias hereditárias.",
+    "alternativa_b": "Na catequização indígena.",
+    "alternativa_c": "No sistema de plantation.",
+    "alternativa_d": "Nas reduções jesuítas.",
+    "alternativa_e": "No tráfico de escravos.",
+    "materia": 2,
+    "autor": 2,
+    "resposta_correta": "A"
+  }
+},
+{
+  "model": "hometask.questao",
+  "pk": "b4a99d42-32d8-49b5-bf82-a7a35c950167",
+  "fields": {
+    "created_at": "2025-10-31T20:14:43.373Z",
+    "updated_at": "2025-10-31T20:14:43.373Z",
+    "enunciado": "(Enem 2022) A nanotecnologia é responsável pelo aprimoramento de diversos materiais, incluindo os que são impactados com a presença de poluentes e da umidade na atmosfera, causadores de corrosão. O processo de corrosão é espontâneo e provoca a deterioração de metais como o ferro, que, em presença de oxigênio e água, sofre oxidação, conforme ilustra a equação química:\r\n\r\n4 Fe (s) + 2 H2O (l) +3 O2 (g) → 2 Fe2O3 · H2O (s)\r\n\r\nUma forma de garantir a durabilidade da estrutura metálica e a sua resistência à umidade consiste na deposição de filmes finos nanocerâmicos à base de zircônia (ZrO2) e alumina (AL2O2) sobre a superfície do objeto que se deseja proteger.\r\n\r\nEssa nanotecnologia aplicada na proteção contra a corrosão se baseia no(a)",
+    "complemento": "",
+    "alternativa_a": "Proteção catódica, que utiliza um metal fortemente redutor.",
+    "alternativa_b": "Uso de metais de sacrifício, que se oxidam no lugar do ferro.",
+    "alternativa_c": "Passivação do ferro, que fica revestido pelo seu próprio óxido.",
+    "alternativa_d": "Efeito de barreira, que impede o contato com o agente oxidante.",
+    "alternativa_e": "Galvanização, que usa outros metais de menor potencial de redução.",
+    "materia": 1,
+    "autor": 2,
+    "resposta_correta": "D"
+  }
+},
+{
+  "model": "hometask.questao",
+  "pk": "d9a0744c-1b5f-419b-a656-62aaaf29a3b0",
+  "fields": {
+    "created_at": "2025-10-31T20:18:04.429Z",
+    "updated_at": "2025-10-31T20:18:04.429Z",
+    "enunciado": "(Enem 2024) O biogás é uma alternativa energética muito importante, pois, além de reduzir a dependência por combustíveis fósseis, sua obtenção pode ser realizada a partir de resíduos da produção agroindustrial. Considere que o biogás produzido em um empreendimento de suinocultura contém 70% em volume de metano (massa molar 16 g/mol; volume molar 22 L/mol). Ele será utilizado para geração de energia em substituição ao etanol (massa molar 46 g/mol) em um gerador no qual 1 m³ de biogás de origem suína substitui 0,59 L de etanol anidro (densidade 0,78 g/mL).\r\n\r\nNessas condições, a massa de metano necessária para substituir 10 mol de etanol na produção de energia é mais próxima de",
+    "complemento": "",
+    "alternativa_a": "300 g.",
+    "alternativa_b": "400 g.",
+    "alternativa_c": "510 g.",
+    "alternativa_d": "590 g",
+    "alternativa_e": "720 g.",
+    "materia": 1,
+    "autor": 2,
+    "resposta_correta": "C"
+  }
+},
+{
+  "model": "hometask.questao",
+  "pk": "dfab7ef8-2ece-4f7c-a277-e863928a2d33",
+  "fields": {
+    "created_at": "2025-10-31T19:26:48.156Z",
+    "updated_at": "2025-10-31T19:39:42.503Z",
+    "enunciado": "(Enem 2024) O magnésio metálico utilizado em ligas leves é produzido em um processo que envolve várias etapas e utiliza água do mar como matéria-prima. A primeira etapa desse processo consiste na reação entre o íon Mg2+ e hidróxido de cálcio, Ca(OH)2, obtendo uma mistura que contém hidróxido de magnésio, pouco solúvel, e íons Ca2+, de acordo com a equação química:\r\n\r\nMg2+ (aq) + Ca(OH)2 (aq) → Mg(OH)2 (s) + Ca2+ (aq)\r\n\r\nO método adequado para separar o Mg(OH)2 dessa mistura é a",
+    "complemento": "",
+    "alternativa_a": "Filtração",
+    "alternativa_b": "Catação",
+    "alternativa_c": "Destilação",
+    "alternativa_d": "Dissolução",
+    "alternativa_e": "Evaporação",
+    "materia": 1,
+    "autor": 2,
+    "resposta_correta": "A"
+  }
+},
+{
+  "model": "hometask.questao",
+  "pk": "e88bbc12-a3ee-4649-8b33-62257ca191a6",
+  "fields": {
+    "created_at": "2025-10-31T19:41:51.295Z",
+    "updated_at": "2025-10-31T19:41:51.295Z",
+    "enunciado": "(Enem 2022) O urânio é empregado como fonte de energia em reatores nucleares. Para tanto, o seu mineral deve ser refinado, convertido a hexafluoreto de urânio e posteriormente enriquecido, para aumentar de 0,7% a 3% a abundância de um isótopo específico — o urânio-235.\r\n\r\nUma das formas de enriquecimento utiliza a pequena diferença de massa entre os hexafluoretos deurânio-235 de urânio-238 para separá-los por efusão, precedida pela vaporização. Esses vapores devem efundir repetidamente milhares de vezes através de barreiras porosas formadas por telas com grande número de pequenos orifícios. No entanto, devido à complexidade e à grande quantidade de energia envolvida, cientistas e engenheiros continuam a pesquisar procedimentos altemativos de enriquecimento, \r\n\r\nConsiderando a diferença de massa mencionada entre os dois isótopos, que tipo de procedimento alternativo ao da efusão pode ser empregado para tal finalidade?",
+    "complemento": "",
+    "alternativa_a": "Peneiração.",
+    "alternativa_b": "Centrifugação.",
+    "alternativa_c": "Extração por solvente.",
+    "alternativa_d": "Destilação fracionada.",
+    "alternativa_e": "Separação magnética",
+    "materia": 1,
+    "autor": 2,
+    "resposta_correta": "B"
+  }
+},
+{
+  "model": "hometask.questao",
+  "pk": "ee8dc643-76ef-4a7c-ab36-60d002318f53",
+  "fields": {
+    "created_at": "2025-10-31T20:24:15.682Z",
+    "updated_at": "2025-10-31T20:24:15.682Z",
+    "enunciado": "(Enem 2020) O movimento sedicioso ocorrido na capitania de Pernambuco, no ano 1817, foi analisado de formas diferentes por dois meios de comunicação daquela época. O Correio Braziliense apontou para o fato de ser “a comoção no Brasil motivada por um descontentamento geral, e não por maquinações de alguns indivíduos”. Já a Gazeta do Rio de Janeiro considerou o movimento como um “pontual desvio de norma, apenas uma 'mancha' nas 'páginas da História Portuguesa\", tão distinta pelos testemunhos de amor e respeito que os vassalos desta nação consagram ao seu soberano”. Os fragmentos das matérias jornalísticas sobre o acontecimento, embora com percepções diversas, relacionam-se a um aspecto do processo de independência da colônia luso-americana expresso em dissensões entre",
+    "complemento": "",
+    "alternativa_a": "Quadros dirigentes em torno da abolição da ordem escravocrata.",
+    "alternativa_b": "Grupos regionais acerca da configuração politico-territorial.",
+    "alternativa_c": "Intelectuais laicos acerca de revogação do domínio eclesiásticos.",
+    "alternativa_d": "Homens livres em torno da extensão do direito de voto.",
+    "alternativa_e": "Elites locais acerca da ordenação do monopólio fundiário.",
+    "materia": 2,
+    "autor": 2,
+    "resposta_correta": "B"
+  }
+}
+]
